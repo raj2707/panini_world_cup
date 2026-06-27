43d03b81eab2522f6dbbbf9cac57353cf3f17edf
Robert Toiu <toiurobert20@gmail.com>
2026-06-27 20:24:20 +0300

    Update stickers+interface

--- app.py
+++ app.py
@@ -2,46 +2,46 @@ import streamlit as st
 import json
 import os
 
-#Citire stickere din .json
+# All the stickers
 with open("stickere.json", "r", encoding="utf-8") as f:
-    stickere_data = json.load(f)
+    stickers_data = json.load(f)
 
-#Progres in .json
-FISIER_PROGRES = "progres.json"
+# Progress
+FILE_PROGRESS = "progres.json"
 
-if os.path.exists(FISIER_PROGRES):
-    with open(FISIER_PROGRES, "r") as f:
-        progres = json.load(f)
+if os.path.exists(FILE_PROGRESS):
+    with open(FILE_PROGRESS, "r") as f:
+        progress = json.load(f)
 else:
-    progres = {}
+    progress = {}
 
-#Interfata
-st.title("Catalog Panini WC 2026 ")
+#Interface
+st.title("Panini WC 2026 ")
 
-total_stickere = 0
-total_colectate = 0
+total_stickers = 0
+total_collected = 0
 
-for echipa, stickere in stickere_data.items():
-    st.subheader(echipa)
+for team, stickers in stickers_data.items():
+    st.subheader(team)
     cols = st.columns(4)
     col_idx = 0
 
-    for cod in stickere:
-        if cod not in progres:
-            progres[cod] = False
+    for cod in stickers:
+        if cod not in progress:
+            progress[cod] = False
         with cols[col_idx % 4]:
-            bifare = st.checkbox(cod, value=progres[cod], key=cod)
-            progres[cod] = bifare
-            if bifare:
-                total_colectate += 1
+            check = st.checkbox(cod, value=progress[cod], key=cod)
+            progress[cod] = check
+            if check:
+                total_collected += 1
         col_idx += 1
-        total_stickere += 1
+        total_stickers += 1
 
-        
-#Contorizare
+
+# Accccounting
 st.divider()
-st.metric("Total colectate", f"{total_colectate} / {total_stickere}")
+st.metric("Total colectate", f"{total_collected} / {total_stickers}")
 
-#Salvare progres
-with open(FISIER_PROGRES, "w") as f:
-    json.dump(progres, f)
+# Saving the progress
+with open(FILE_PROGRESS, "w") as f:
+    json.dump(progress, f)
